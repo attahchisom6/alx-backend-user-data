b8be18d9370347f29afcd35f231d9598276c5fcf
attahchisom6 <attahchisom6@gmail.com>
2023-08-08 09:13:59 +0100

    user password and email

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,3 +45,23 @@ class BasicAuth(Auth):
             return None
 
         return decoded_bytes.decode("utf-8")
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+        ) -> (str, str):#<F11>
+        """
+        method that returns the user email and password
+        from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        listt = decoded_base64_authorization_header.split(":")
+        return (listt[0], listt[1])
