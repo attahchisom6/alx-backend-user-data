65df32a6a0f6fa8dc9ad5286bfc54bcd3f452fc1
attahchisom6 <attahchisom6@gmail.com>
2023-08-08 11:21:46 +0100

    get a uder by his email or passeord

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ A child class of the auth class
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -64,3 +66,31 @@ class BasicAuth(Auth):
 
         listt = decoded_base64_authorization_header.split(":")
         return (listt[0], listt[1])
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+        that returns the User instance based on his email and password
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        user = User()
+        users_list = user.search({"email": user_email})
+        if users_list is None:
+            return None
+
+        user_with_valid_pwd = []
+        try:
+            for user in users_list:
+                if user.is_valid_password(user_pwd):
+                    user_with_valid_pwd.append(user)
+
+            return user_with_valid_pwd[0]
+        except Exception:
+            return None
