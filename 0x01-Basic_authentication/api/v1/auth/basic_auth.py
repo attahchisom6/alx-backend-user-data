1db9094984f84f1de66a31eb38ec8135fce99b61
attahchisom6 <attahchisom6@gmail.com>
2023-08-08 02:23:53 +0100

    decode base64 to utf-8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,8 +26,9 @@ class BasicAuth(Auth):
         value = authorization_header[6:]
         return value
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
         """
         method that returns the decoded value of a Base64 string
         base64_authorization_header
