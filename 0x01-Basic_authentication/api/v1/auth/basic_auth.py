193ba4e67fc07c0297a44432d8ff8a3bb93a2ca5
attahchisom6 <attahchisom6@gmail.com>
2023-08-08 01:41:59 +0100

    get a base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,18 @@ class BasicAuth(Auth):
     """
     a child authentication class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        method  that returns the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+            value = authorization_header[6:]
+            return value
