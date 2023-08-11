9479fa6ce693780e92471f2de17e433e4d7d1bb8
attahchisom6 <attahchisom6@gmail.com>
2023-08-11 23:55:42 +0100

    qdd

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -43,7 +43,7 @@ class SessionDBAuth(SessionExpAuth):
         UserSession.load_from_file()
         user_sessions = UserSession.search({"session_id": session_id})
 
-        if user_sessions is None:
+        if not user_sessions:
             return None
 
         # we use the first item in the list becos we believe that the list
@@ -56,8 +56,7 @@ class SessionDBAuth(SessionExpAuth):
         if session_time < current_time:
             return None
 
-        user_id = user_sessions[0].user_id
-        return user_id
+        return user_sessions[0].user_id
 
     def destroy_session(self, request=None):
         """
