df45f60a04fc0de0dbb2bc9a59b700505d287889
attahchisom6 <attahchisom6@gmail.com>
2023-08-11 19:36:41 +0100

    add

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-""" User module
+#!/usrlbin/env python3
+"""
+Module made to store user sessions so that no ids are lost after each
+session
 """
 from models.base import Base
 
 
 class UserSession(Base):
-    """ User session class
     """
-
+    User class to handle session storage
+    """
     def __init__(self, *args: list, **kwargs: dict):
-        """ The constructor
+        """
+        initalizing instances for user_id and session id
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
