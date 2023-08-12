41e9b64691caeacc004814c87f12582b51564499
attahchisom6 <attahchisom6@gmail.com>
2023-08-12 10:27:24 +0000

    add

---
+++ 0x0A-test/api/v1/auth/session_db_auth.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+module for session storage
+"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """
+    store user data and session to a file storage
+    """
+    def create_session(self, user_id=None):
+        """
+        overloads the original method from the parent, return its own session
+        """
+        if user_id is None:
+            return None
+
+        session_id = super().create_session(user_id)
+
+        if session_id is None:
+            return None
+
+        session_dictionary = {
+                "user_id": user_id,
+                "session_id": session_id
+            }
+
+        user_session = UserSession(**session_dictionary)
+        user_session.save()
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        retrieve user id from its session_id for storage
+        """
+        if session_id is None:
+            return None
+
+        UserSession.load_from_file()
+        user_sessions = UserSession.search({"session_id": session_id})
+
+        if user_sessions is None:
+            return None
+
+        # we use the first item in the list becos we believe that the list
+        # returned only one unique session_id that we wantd
+        created_at = user_sessions[0].created_at
+
+        current_time = datetime.utcnow()
+        session_time = created_at + timedelta(seconds=self.session_duration)
+
+        if session_time < current_time:
+            return None
+
+        user_id = user_sessions[0].user_id
+        return user_id
+
+    def destroy_session(self, request=None):
+        """
+        destroy a user bases on the session id
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_sessions = UserSession.search({"session_id": session_id})
+
+        if user_sessions is None:
+            return False
+
+        user_sessions[0].remove()
+        return True
---
+++ 0x0A-test/work_dir/7.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+A view for all session authenication routes
+"""
+from flask import request, jsonify
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
+def creat_user_login():
+    """
+    method to handles user login using session authentication
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    email_error = {"error": "email missing"}
+    pwd_error = {"error": "password missing"}
+
+    if email is None or email == "":
+        return jsonify(email_error), 400
+
+    if password is None or password == "":
+        return jsonify(pwd_error), 400
+
+    user = User()
+    user_list = user.search({"email": email})
+    if not user_list:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    from api.v1.app import auth
+    users_with_password = []
+    for users in user_list:
+        if not users.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        else:
+            users_with_password.append(users)
+    user = users_with_password[0]
+
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+
+    session_name = os.getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+
+    return response
