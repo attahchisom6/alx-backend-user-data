a0130dce9c3f85db85474b3efcf02d6eda70a828
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 10:14:12 +0100

    add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -13,20 +13,23 @@ from user import Base, User
 
 
 class DB:
-    """DB class
     """
-
+    storage class
+    """
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """
+        inittializing storage engine
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
+
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+        a getter method to return a session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -45,7 +48,7 @@ class DB:
 
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: dict) -> User:
         """
         search the user table and returns the user in the first row whose
         attribute mathes key-values in kwargs
@@ -56,9 +59,25 @@ class DB:
         # a property method is called without a parenthesis
         # db = self._session
         try:
-            user = self._session.query(User).filter_by(**kwargs).first()
+            users = self._session.query(User)
+            user = users.filter_by(**kwargs).first()
         except TypeError:
             raise InvalidRequestError
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """
+        method that locates a user and updates a its attribute
+        """
+        db = self._session
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                setattr(user, key, value)
+            db.commit()
+        except Exception:
+            raise ValueError
