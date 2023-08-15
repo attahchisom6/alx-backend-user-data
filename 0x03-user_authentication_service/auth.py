fc61a43b4f4b2ab8984c39ba010293058b356996
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 12:51:37 +0100

    addes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,3 +35,14 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        checks if the user instance loggin in is a valid user
+        """
+        user = self.find_user_by(email=email)
+        if user:
+            is_valid = bcrypt.checkpw(password.encode("utf-8"), password)
+            if is_valid:
+                return True
+        return False
