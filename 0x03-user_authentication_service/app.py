f0deeeeba8b97d1a138d2038ef24ef11c3fab1b1
attahchisom6 <attahchisom@gmail.com>
2023-08-15 23:53:52 +0100

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,17 +18,18 @@ def welcome_message() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
+    """Method that registers a user.
+
+    Returns:
+        string : JSON payload
     """
-    a route/endpoint to register users
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
+    email = request.form.get('email')
+    password = request.form.get('password')
     try:
-        user = Auth.register_user(email, password)
-        if user is not None:
-            return jsonify({"email": user.email, "message": "user created"})
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
