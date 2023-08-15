1c9400e25bd38a5c60700922cd390a8b4db75d7d
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 11:41:55 +0100

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,7 +23,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """
         register a user and save to the database
         """
