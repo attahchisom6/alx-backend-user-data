dc1356b3da533aebb874cdb73ed53d1342d06305
attahchisom6 <attahchisom6@gmail.com>
2023-08-16 00:51:20 +0100

    add

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_by_session_id(self, session_id: str) -> User:
+        """
+        get a user corresponding to a given session id
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+
+            return user
+        except NoResultFound:
+            return None
