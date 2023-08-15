a543949bb02af75e856e1e95ccdd0f1398574790
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 10:38:33 +0100

    add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -67,7 +67,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
         method that locates a user and updates a its attribute
         """
