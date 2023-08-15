ab988427a7f3aacfaa7f7eba8735ede11c14033b
attahchisom6 <attahchisom@gmail.com>
2023-08-15 23:36:03 +0100

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,8 +26,8 @@ def users() -> str:
     email = request.form.get("email")
     password = request.form.get("password")
     try:
+        user = Auth.register_user(email, password)
         if user is not None:
-            user = Auth.register_user(email, password)
             return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
