9f1c5b33e647b0b287e0c624b6a61fb1cca9836c
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 10:02:20 +0100

    add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -13,23 +13,20 @@ from user import Base, User
 
 
 class DB:
+    """DB class
     """
-    storage class
-    """
-    def __init__(self) -> None:
-        """
-        inittializing storage engine
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
 
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """
-        a getter method to return a session
+        """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -65,18 +62,3 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
-
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
-        """
-        method that locates a user and updates a its attribute
-        """
-        db = self._session
-        try:
-            user = self.find_user_by(id=user_id)
-            for key, value in kwargs.items():
-                if not hasattr(user, key):
-                    raise ValueError
-                setattr(user, key, value)
-            db.commit()
-        except Exception:
-            raise ValueError
