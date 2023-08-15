8ac3a7ebca18b18d0696f97842a3573c712c3af8
attahchisom6 <attahchisom6@gmail.com>
2023-08-15 09:55:18 +0100

    add

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,61 +1,82 @@
 #!/usr/bin/env python3
-""" DB module to save and update databse
+
+"""DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """ DB class """
-
+    """
+    storage class
+    """
     def __init__(self) -> None:
-        """ Initialize a new DB instance """
-        self._engine = create_engine("sqlite:///a.db")
+        """
+        inittializing storage engine
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """ Memoized session object """
+        """
+        a getter method to return a session
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to db """
+        """
+        add and saves a user to the database
+        """
+        db = self._session
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+
+        db.add(new_user)
+        db.commit()
+
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
-        """ Returns first row found in users table based on keyword args """
-
+    def find_user_by(self, **kwargs: dict) -> User:
+        """
+        search the user table and returns the user in the first row whose
+        attribute mathes key-values in kwargs
+        Args:
+            kwargs: the search parameter
+        Return: the user
+        """
+        # a property method is called without a parenthesis
+        # db = self._session
         try:
-            record = self._session.query(User).filter_by(**kwargs).first()
+            user = self._session.query(User).filter_by(**kwargs).first()
         except TypeError:
             raise InvalidRequestError
-        if record is None:
+        if user is None:
             raise NoResultFound
-        return record
+        return user
 
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """ Finds user record and updates attributes """
-        user_record = self.find_user_by(id=user_id)
-
-        for key, value in kwargs.items():
-            if hasattr(user_record, key):
-                setattr(user_record, key, value)
-            else:
-                raise ValueError
-
-        self._session.commit()
-        return None
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """
+        method that locates a user and updates a its attribute
+        """
+        db = self._session
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                setattr(user, key, value)
+            db.commit()
+        except Exception:
+            raise ValueError
