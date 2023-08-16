9e4ff2f91edeb9be8b63dab1be0eeaefc09331a9
attahchisom6 <attahchisom6@gmail.com>
2023-08-16 16:10:47 +0100

    fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -123,11 +123,9 @@ class Auth:
             raise ValueError
 
         hashed_password = _hash_password(password)
-        """kwargs = {
+        kwargs = {
                 "hashed_password": hashed_password,
                 "reset_token": None
-            }"""
+            }
 
-        self._db.update_user(user.id,
-                             hashed_password=hashed_password,
-                             reset_token=None)
+        self._db.update_user(user.id, **kwargs)
