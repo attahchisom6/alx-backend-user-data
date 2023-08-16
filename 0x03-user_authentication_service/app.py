9cf873dca49b631665bb5b87c0d0abafa6177722
attahchisom6 <attahchisom6@gmail.com>
2023-08-16 12:47:35 +0100

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,7 +83,7 @@ def user_profile():
     """
     session_id = request.cookies.get("session_id")
     try:
-        user = Auth.get_user_from_session_id(session_id)
+        user = AUTH.get_user_from_session_id(session_id)
         if user is None:
             abort(403)
         return jsonify({"email": user.email}), 200
