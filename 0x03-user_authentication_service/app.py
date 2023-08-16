549ce7c645549908dcf15e2f8c4225adb4a8092b
attahchisom6 <attahchisom6@gmail.com>
2023-08-16 12:16:00 +0100

    add

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,5 +76,20 @@ def logout() -> None:
         abort(403)
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def user_profile():
+    """
+    get the user or user using a session id
+    """
+    try:
+        session_id = request.cookies.get("session_id")
+        user = Auth.get_user_from_session_id(session_id)
+        if user is None:
+            abort(403)
+        return jsonify({"email": user.email}), 200
+    except (NoResultFound, TypeError):
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
